8c79943065426c3f5cdea4d7f656ff13d2395858
felix.lin <fly_81211@hotmail.com>
2016-06-26 14:29:03 +0800

    Initial Commit

---
+++ FLTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FLTabBar
+//
+//  Created by LBH on 2016/6/26.
+//  Copyright © 2016年 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FLTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FLTabBar
+//
+//  Created by LBH on 2016/6/26.
+//  Copyright © 2016年 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
